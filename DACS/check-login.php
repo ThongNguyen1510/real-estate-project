f745b5116204b8bc2c84afd277d70da7625b200c
PhoAnLien <thanhpham.apr@gmail.com>
2025-05-02 12:37:29 +0700

    Delete DACS directory

--- DACS/check-login.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['user'])) {  // Thay đổi điều kiện kiểm tra
-    header('Location: login.php'); // Chuyển hướng đến trang đăng nhập nếu chưa đăng nhập
-    exit;
-}
-?>
